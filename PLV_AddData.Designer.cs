70c6d791a5e65d5dcacfd296735404c7409fece3
joshuadapitan <joshuadapitan@gmail.com>
2015-04-27 12:52:14 +0800

    Revert "Revert "Database Design Revisions+GUI Revisions""
    This reverts commit cd821eacea73753383e00b6db340b375e44e603e.

--- PLV_AddData.Designer.cs
+++ PLV_AddData.Designer.cs
@@ -44,6 +44,8 @@
             this.rbtWhitePrint = new System.Windows.Forms.RadioButton();
             this.rbtTracingCloth = new System.Windows.Forms.RadioButton();
             this.rbtOriginal = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cbxPlanType
@@ -55,7 +57,7 @@
             "(LRC)PCN",
             "(LRC)RL",
             "(LRC)RS"});
-            this.cbxPlanType.Location = new System.Drawing.Point(96, 20);
+            this.cbxPlanType.Location = new System.Drawing.Point(99, 89);
             this.cbxPlanType.Margin = new System.Windows.Forms.Padding(4);
             this.cbxPlanType.Name = "cbxPlanType";
             this.cbxPlanType.Size = new System.Drawing.Size(160, 24);
@@ -64,7 +66,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Location = new System.Drawing.Point(16, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(81, 17);
             this.label1.TabIndex = 2;
@@ -73,7 +75,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 56);
+            this.label2.Location = new System.Drawing.Point(30, 125);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 17);
             this.label2.TabIndex = 3;
@@ -81,7 +83,7 @@
             // 
             // txtPlanNo
             // 
-            this.txtPlanNo.Location = new System.Drawing.Point(96, 56);
+            this.txtPlanNo.Location = new System.Drawing.Point(99, 125);
             this.txtPlanNo.Name = "txtPlanNo";
             this.txtPlanNo.Size = new System.Drawing.Size(100, 22);
             this.txtPlanNo.TabIndex = 4;
@@ -89,7 +91,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(31, 91);
+            this.label3.Location = new System.Drawing.Point(34, 160);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 17);
             this.label3.TabIndex = 5;
@@ -97,7 +99,7 @@
             // 
             // txtRollNo
             // 
-            this.txtRollNo.Location = new System.Drawing.Point(96, 86);
+            this.txtRollNo.Location = new System.Drawing.Point(99, 155);
             this.txtRollNo.Name = "txtRollNo";
             this.txtRollNo.Size = new System.Drawing.Size(100, 22);
             this.txtRollNo.TabIndex = 6;
@@ -105,7 +107,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 120);
+            this.label4.Location = new System.Drawing.Point(26, 189);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(71, 17);
             this.label4.TabIndex = 7;
@@ -113,7 +115,7 @@
             // 
             // txtLocation
             // 
-            this.txtLocation.Location = new System.Drawing.Point(96, 120);
+            this.txtLocation.Location = new System.Drawing.Point(99, 189);
             this.txtLocation.Name = "txtLocation";
             this.txtLocation.Size = new System.Drawing.Size(160, 22);
             this.txtLocation.TabIndex = 8;
@@ -121,7 +123,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(23, 156);
+            this.label5.Location = new System.Drawing.Point(26, 225);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(68, 17);
             this.label5.TabIndex = 9;
@@ -129,7 +131,7 @@
             // 
             // txtRemarks
             // 
-            this.txtRemarks.Location = new System.Drawing.Point(96, 151);
+            this.txtRemarks.Location = new System.Drawing.Point(99, 220);
             this.txtRemarks.Name = "txtRemarks";
             this.txtRemarks.Size = new System.Drawing.Size(160, 22);
             this.txtRemarks.TabIndex = 10;
@@ -137,7 +139,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(31, 187);
+            this.label6.Location = new System.Drawing.Point(34, 256);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(49, 17);
             this.label6.TabIndex = 11;
@@ -145,7 +147,7 @@
             // 
             // btnInsert
             // 
-            this.btnInsert.Location = new System.Drawing.Point(96, 295);
+            this.btnInsert.Location = new System.Drawing.Point(99, 364);
             this.btnInsert.Name = "btnInsert";
             this.btnInsert.Size = new System.Drawing.Size(75, 34);
             this.btnInsert.TabIndex = 12;
@@ -156,7 +158,7 @@
             // rbtBluePrint
             // 
             this.rbtBluePrint.AutoSize = true;
-            this.rbtBluePrint.Location = new System.Drawing.Point(96, 241);
+            this.rbtBluePrint.Location = new System.Drawing.Point(99, 310);
             this.rbtBluePrint.Name = "rbtBluePrint";
             this.rbtBluePrint.Size = new System.Drawing.Size(90, 21);
             this.rbtBluePrint.TabIndex = 13;
@@ -167,7 +169,7 @@
             // rbtWhitePrint
             // 
             this.rbtWhitePrint.AutoSize = true;
-            this.rbtWhitePrint.Location = new System.Drawing.Point(96, 268);
+            this.rbtWhitePrint.Location = new System.Drawing.Point(99, 337);
             this.rbtWhitePrint.Name = "rbtWhitePrint";
             this.rbtWhitePrint.Size = new System.Drawing.Size(98, 21);
             this.rbtWhitePrint.TabIndex = 14;
@@ -178,7 +180,7 @@
             // rbtTracingCloth
             // 
             this.rbtTracingCloth.AutoSize = true;
-            this.rbtTracingCloth.Location = new System.Drawing.Point(96, 214);
+            this.rbtTracingCloth.Location = new System.Drawing.Point(99, 283);
             this.rbtTracingCloth.Name = "rbtTracingCloth";
             this.rbtTracingCloth.Size = new System.Drawing.Size(113, 21);
             this.rbtTracingCloth.TabIndex = 15;
@@ -189,7 +191,7 @@
             // rbtOriginal
             // 
             this.rbtOriginal.AutoSize = true;
-            this.rbtOriginal.Location = new System.Drawing.Point(96, 187);
+            this.rbtOriginal.Location = new System.Drawing.Point(99, 256);
             this.rbtOriginal.Name = "rbtOriginal";
             this.rbtOriginal.Size = new System.Drawing.Size(78, 21);
             this.rbtOriginal.TabIndex = 16;
@@ -197,11 +199,32 @@
             this.rbtOriginal.Text = "Original";
             this.rbtOriginal.UseVisualStyleBackColor = true;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "*-Required Fields";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(13, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(201, 36);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Add new Plan";
+            // 
             // PLV_AddData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 356);
+            this.ClientSize = new System.Drawing.Size(272, 407);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.rbtOriginal);
             this.Controls.Add(this.rbtTracingCloth);
             this.Controls.Add(this.rbtWhitePrint);
@@ -219,7 +242,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbxPlanType);
             this.Name = "PLV_AddData";
-            this.Text = "PLV_AddData";
+            this.Text = "pLaiRes Emulator";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PLV_AddData_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -244,5 +267,7 @@
         private System.Windows.Forms.RadioButton rbtWhitePrint;
         private System.Windows.Forms.RadioButton rbtTracingCloth;
         private System.Windows.Forms.RadioButton rbtOriginal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
